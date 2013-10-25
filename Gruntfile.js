48b9dd6b415db438d4b960908de7a174fa23f349
Joseph Spens <joseph@workmarket.com>
2013-10-25 09:37:30 -0400

    Edited gruntfile so it only copies font files into dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -313,7 +313,7 @@ module.exports = function (grunt) {
 						'.htaccess',
 						'images/{,*/}*.{webp,gif}',
 						'styles/fonts/{,*/}*.*',
-						'bower_components/sass-bootstrap/fonts/*.*'
+						'bower_components/sass-bootstrap/fonts/*.{eot,woff,ttf,svg}'
 					]
 				},
 				{
