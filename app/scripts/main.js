16824769e49a0a247694a8e2e2bcd85a89f14b85
Joseph Spens <joseph@workmarket.com>
2013-10-29 21:27:16 -0400

    Added font-awesome

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,10 @@
 require.config({
 	paths: {
 		jquery: '../bower_components/jquery/jquery',
-		howler: '../bower_components/howler/howler'
+		howler: '../bower_components/howler/howler',
+		requirejs: '../bower_components/requirejs/require',
+		'handlebars-latest': '../bower_components/handlebars-latest.js/index',
+		handlebars: '../bower_components/handlebars.js/dist/handlebars'
 	}
 });
 
