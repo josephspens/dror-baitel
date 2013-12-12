712f01c2731494b2db190bd3fbb0fe0fe34ccff8
Joseph Spens <joseph@workmarket.com>
2013-12-11 22:19:03 -0500

    Adding dist back in

--- server.js
+++ server.js
@@ -5,9 +5,13 @@
 // server.listen();
 
 var express = require("express");
+var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+app.configure(function(){
+	app.use(express.bodyParser());
+	app.use(express.static(path.join(__dirname, 'dist')));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
